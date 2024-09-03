2553d327f0f9ffaa54cc1245f9c374c64ac968c5
mtaut <146488331+mtaut@users.noreply.github.com>
2024-09-02 21:48:25 -0500

    Update EndToEnd.test.js

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,6 +1,40 @@
 import puppeteer from "puppeteer";
 //  import jest here?
 
+// Feature 1: Filter Events by City
+describe("filter events by city", () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  test("When user hasn't searched for a city, show upcoming events from all cities", async () => {
+    await page.waitForSelector(".event");
+    const events = await page.$(".event");
+    expect(events).toBeDefined();
+  });
+
+  test("User should see a list of suggestions when they search for a city", async () => {
+    await page.type(".city", "Berlin");
+    const suggestions = await page.$(".suggestions");
+    expect(suggestions).toBeDefined();
+  });
+
+  test("User can select a city from the suggested list", async () => {
+    await page.click(".suggestions li");
+    const city = await page.$(".city");
+    expect(city).toBeDefined();
+  });
+});
+
+// Feature 2: Show/Hide Event Details
 describe("show/hide event details", () => {
   let browser;
   let page;
