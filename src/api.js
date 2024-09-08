ae194b58c0a66d99fb517d9d39459ab1caef58d0
mtaut <146488331+mtaut@users.noreply.github.com>
2024-09-08 15:54:00 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -68,7 +68,9 @@ export const getAccessToken = async () => {
  *
  */
 export const getEvents = async () => {
+  NProgress.start();
   if (window.location.href.startsWith("http://localhost")) {
+    NProgress.done();
     return mockData;
   }
 
