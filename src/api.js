4e3aabaa1b4d6e297dd757c830f1e8e242ad17c9
mtaut <146488331+mtaut@users.noreply.github.com>
2024-09-08 15:41:04 -0500

    Update api.js
    added offline capability

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 import mockData from "./mock-data";
+import NProgress from "nprogress";
 
 export const extractLocations = (events) => {
   const extractedLocations = events.map((event) => event.location);
@@ -71,6 +72,12 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events ? JSON.parse(events) : [];
+  }
+
   const token = await getAccessToken();
 
   if (token) {
@@ -82,6 +89,8 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
   }
