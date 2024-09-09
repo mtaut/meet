112d2139a719369d05726465190b92a140dceb57
mtaut <146488331+mtaut@users.noreply.github.com>
2024-09-08 22:22:29 -0500

    ex4.9

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
 import NumberOfEvents from "./components/NumberOfEvents";
 import "./App.css";
-import { InfoAlert, ErrorAlert } from "./components/Alert";
+import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert";
 
 const App = () => {
   const [allLocations, setAllLocations] = useState([]);
@@ -13,8 +13,15 @@ const App = () => {
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState("");
+  const [warningAlert, setWarningAlert] = useState("");
 
   useEffect(() => {
+    if (navigator.onLine) {
+      setWarningAlert("");
+    } else {
+      setWarningAlert("You are offline. The last saved events will be loaded.");
+    }
+
     const fetchData = async () => {
       const allEvents = await getEvents();
       const filteredEvents =
@@ -24,7 +31,6 @@ const App = () => {
       setEvents(filteredEvents.slice(0, currentNOE));
       setAllLocations(extractLocations(allEvents));
     };
-
     fetchData();
   }, [currentCity, currentNOE]);
 
@@ -35,6 +41,7 @@ const App = () => {
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
       </div>
       <CitySearch
         allLocations={allLocations}
